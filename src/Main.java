8ce3751733286b147121b1540f212bf86967bfbe
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 23:34:06 +0100

    Functions outsourced

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,38 @@ public class Main {
     public static final String SIGN = "*";
     public static final String SPACE = " ";
     public static final String MESSAGE = "Kérem a piramis magasságát: ";
-    public static final String ERRORMESSAGE = "A piramis magassága nem lehet kisebb, mint 1!";
+    public static final int MINIMALHEIGHT = 2; // 1-el még működik, de az már nem piramis, hanem csak 1 *
+    public static final String ERRORMESSAGE = "A piramis magassága nem lehet kisebb, mint " + MINIMALHEIGHT + "!";
+
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         /*
             piramis rajzoló
             kérjen a felhasználótól egy számot és rajzoljon olyan magasságú teljes piramis formát
         */
-
-        // spagetti kód
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(MESSAGE);
-        int height = scanner.nextInt();
-        if (height < 1) {
+        int height = getUserInput(MESSAGE);
+        if (height < MINIMALHEIGHT) {
             System.out.println(ERRORMESSAGE);
         } else {
-            for (int i = 0; i < height; i++) {
-                for (int j = i; j < (height - 1); j++) {
-                    System.out.print(SPACE);
-                }
-                for (int k = 0; k <= (i * 2); k++) {
-                    System.out.print(SIGN);
-                }
-                System.out.println();
-            }
+            drawPyramid(height);
         }
+    }
 
+    public static int getUserInput(String message) {
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
+    public static void drawPyramid(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = i; j < (height - 1); j++) {
+                System.out.print(SPACE);
+            }
+            for (int k = 0; k <= (i * 2); k++) {
+                System.out.print(SIGN);
+            }
+            System.out.println();
+        }
     }
 }
