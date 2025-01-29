6f69b11fd327148048f51d0792c69e7b70714655
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 23:23:21 +0100

    Constants introduced

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static final String SIGN = "*";
+    public static final String SPACE = " ";
+    public static final String MESSAGE = "Kérem a piramis magasságát: ";
+    public static final String ERRORMESSAGE = "A piramis magassága nem lehet kisebb, mint 1!";
+
     public static void main(String[] args) {
         /*
             piramis rajzoló
@@ -10,17 +15,17 @@ public class Main {
 
         // spagetti kód
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Kérem a piramis magasságát: ");
+        System.out.print(MESSAGE);
         int height = scanner.nextInt();
         if (height < 1) {
-            System.out.println("A piramis magassága nem lehet kisebb, mint 1!");
+            System.out.println(ERRORMESSAGE);
         } else {
             for (int i = 0; i < height; i++) {
                 for (int j = i; j < (height - 1); j++) {
-                    System.out.print(" ");
+                    System.out.print(SPACE);
                 }
                 for (int k = 0; k <= (i * 2); k++) {
-                    System.out.print("*");
+                    System.out.print(SIGN);
                 }
                 System.out.println();
             }
